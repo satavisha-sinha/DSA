07a2eabb5b8d5e5366ffda2669e3023e070e5503
satavish-sinha <satavishasinha123@gmail.com>
2026-07-18 23:30:44 +0530

    without using i+1

--- Recursion/Problem3.cpp
+++ Recursion/Problem3.cpp
@@ -1,10 +1,12 @@
+// Problem 2 but without using i+1
+
 #include<bits/stdc++.h>
 using namespace std;
 
 void print_n(int i){
     if(i<1) return ;
-    cout<<i<<" ";
     print_n(i-1); 
+    cout<<i<<" "; 
 }
 int main(){
     int n;
